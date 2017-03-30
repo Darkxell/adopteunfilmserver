154b4499bebaa07b129f26a988651d583f4ba670
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-03-30 19:35:00 +0200

    Tsssskambadam

--- src/main/java/adopteunfilmserver/recommendationalgorithm/ProximityRules.java
+++ src/main/java/adopteunfilmserver/recommendationalgorithm/ProximityRules.java
@@ -3,15 +3,15 @@ package adopteunfilmserver.recommendationalgorithm;
 /** Stores the score value of the different rules for quick modifications. */
 enum ProximityRules {
 
-	MATCHING_KEYWORD(25),
-	MATCHING_GENRE(100),
-	MATCHING_TYPE(150),
+	MATCHING_KEYWORD(500),
+	MATCHING_GENRE(1000),
+	MATCHING_TYPE(10),
 	MATCHING_ACTOR(15),
 	
-	YEAR_DIFF_MAX(250),// year matching score is = MAX - (diff*multiplier)
-	YEAR_DIFF_MULTIPLIER(25),
+	YEAR_DIFF_MAX(2500),// year matching score is = MAX - (diff*multiplier)
+	YEAR_DIFF_MULTIPLIER(500),
 	
-	MATCHING_LANGUAGE(50),
+	MATCHING_LANGUAGE(20),
 	MATCHING_PRODUCER(200),
 	MATCHING_DIRECTOR(300),
 	MATCHING_COMPANY(400),
